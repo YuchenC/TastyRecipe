45f964649955aaf2bf5311417bc33ab4c490c131
YuchenC <yuchenc@kth.se>
2018-05-31 13:35:10 +0200

    resources/fragments

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tasty Recipe</title>
+        <link rel="stylesheet" type="text/css" href="resources/css/menu.css" />
+    </head>
+    <body>
+        <ul>
+            <li><a href="index.php">Home</a></li>        
+            <li><a href="to-calendar.php">Calendar</a></li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Recipe</a>
+                <div class="dropdown-content">
+                    <a href="to-first-recipe.php">Mozzarella & prosciutto panini</a>
+                    <a href="to-second-recipe.php">Winter crunch salad</a>
+                    <a href="to-third-recipe.php">Meatballs</a>
+                    <a href="to-fourth-recipe.php">Pancake</a>
+                </div>
+            <li><a href="to-login.php">Log in</a></li>
+            <li><a href="to-register.php">Register</a></li>
+            </li>  
+        </ul>
+    </body>
+</html>
